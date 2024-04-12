80c65b69dcb79f3fcaf42a5f068c3c0ca2a22596
Ivan Podgornov <ext.ipodgornov@tinkoff.ru>
2024-04-13 02:43:46 +0300

    chore: настройка stylelint

--- packages/core/components/props-editor/props-editor.tsx
+++ packages/core/components/props-editor/props-editor.tsx
@@ -1,12 +1,14 @@
 import React, { type FC } from 'react';
 
+import styles from './props-editor.css';
+
 export interface Props {
   schema: Record<string, string>;
 }
 
 export const PropsEditor: FC<Props> = ({ schema }) => {
   return (
-    <label>
+    <label className={styles.root}>
       <span>Props:</span>
       <textarea defaultValue={JSON.stringify(schema, null, 2)} />
     </label>
