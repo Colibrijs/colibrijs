106c7406106340be165c1d7d66a58c3e0705f856
Ivan Podgornov <ivan.podgornov@live.com>
2024-04-14 21:15:56 +0300

    feat: добавлен пример микрофронетнда

---
+++ packages/types/types.ts
@@ -0,0 +1,14 @@
+import type { ComponentType } from 'react';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment -- осознанно пока что игнорируется параметр P
+// @ts-expect-error -- осознанно пока что игнорируется параметр P
+export type JsonSchema<P> = string; // eslint-disable-line @typescript-eslint/no-unused-vars -- осознанно пока что игнорируется параметр P
+
+export interface MicrofrontendMeta<P> {
+  /** Реализация компонента */
+  component: ComponentType<P>;
+  /** Название */
+  name: string;
+  /** Схема пропсов компонента */
+  schema: JsonSchema<P>;
+}
