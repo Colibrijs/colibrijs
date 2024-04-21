de87d108b9e41f57a3d12e996015c716d4454300
Ivan Podgornov <ivan.podgornov@live.com>
2024-04-21 23:33:32 +0300

    ci: настройка playwright

---
+++ packages/admin/next.config.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires -- не разрешает next писать конфиг на ts
+const { UniversalFederationPlugin } = require('@module-federation/node');
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  transpilePackages: ['@colibrijs/core'],
+
+  env: {
+    EXAMPLE_URL: process.env.EXAMPLE_URL || 'https://colibrijs.github.io/colibrijs/main/example/',
+  },
+
+  webpack: (config, context) => {
+    config.plugins.push(
+      new UniversalFederationPlugin(
+        {
+          isServer: context.isServer,
+          library: { name: 'admin', type: 'commonjs-module' },
+          remotes: {
+            fake: 'promise new Promise((resolve) => {resolve({get:()=>Promise.resolve(()=>{}),init:()=>{}})})',
+          },
+          name: 'admin',
+          shared: {
+            react: {
+              eager: true,
+              import: 'react',
+              requiredVersion: '^18',
+              shareKey: 'react',
+              shareScope: 'default',
+              singleton: true,
+            },
+          },
+        },
+        {}
+      )
+    );
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
