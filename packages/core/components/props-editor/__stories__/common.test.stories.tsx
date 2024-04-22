de5781d836fc344fc66e9a45e7ff2beb55715585
okovtkov <okovtkov@gmail.com>
2024-04-22 23:41:29 +0200

    fix: поправлены тесты

--- packages/core/components/props-editor/__stories__/common.test.stories.tsx
+++ packages/core/components/props-editor/__stories__/common.test.stories.tsx
@@ -3,55 +3,46 @@ import { expect, within, userEvent, fn } from '@storybook/test';
 import PropsEditorStoryMeta, { type PropsEditorMeta, type Story } from './props-editor.stories';
 import schema from './schema.json';
 
-import { getPropertiesNames } from '../utils/get-properties-names';
-
 export default {
   ...PropsEditorStoryMeta,
-  title: 'PropsEditor/tests/callbacks',
+  title: 'PropsEditor/tests/common',
 } satisfies PropsEditorMeta;
 
 export const ChangeEvent: Story = {
-  name: 'При изменении поля, происходит вызов onChange с нужными данными',
+  name: 'При изменении поля, происходит вызов onChange с введенными данными',
   play: async ({ args, canvasElement, step }) => {
     const { getAllByTestId } = within(canvasElement);
-    const inputs = getAllByTestId('props-editor__input');
-    const firstInput = inputs[0];
-    if (!firstInput) throw Error('Не найден инпут');
+    const firstInput = getAllByTestId('props-editor__input')[0]!;
 
     await step(
       'Вводим слово "Кек" в первый инпут',
       async () => await userEvent.type(firstInput, 'Кек')
     );
 
-    await step('Проверем, что пропс onChange был вызван с корректными данными', async () => {
+    await step('Проверем, что пропс onChange был вызван с введенными данными', async () => {
       await expect(args.onChange).toHaveBeenCalledWith({ name: 'Кек', surname: '' });
     });
   },
 };
 
 export const LabelFields: Story = {
-  name: 'При передаче схемы, отрисовываются корректные поля с лейблами',
-  play: async ({ args, canvasElement, step }) => {
+  name: 'При передаче схемы, отрисовываются поля с лейблами из JSON-схемы',
+  play: async ({ canvasElement, step }) => {
     const { getAllByTestId } = within(canvasElement);
     const items = getAllByTestId('props-editor__item');
 
-    const keys = getPropertiesNames(args.schema);
     await step('Ищем все поля и убеждаемся что лейблы соответствуют тем, что в схеме', () => {
-      items.forEach((item, index) => {
-        const { getByTestId } = within(item);
-        const label = getByTestId('props-editor__label');
-        const labelText = label?.textContent?.trim();
-        const input = getByTestId('props-editor__input');
+      const nameLabel = within(items[0]!).getByTestId('props-editor__label');
+      const surnameLabel = within(items[1]!).getByTestId('props-editor__label');
 
-        expect(labelText).toBe(keys[index]);
-        expect(input).toBeInTheDocument();
-      });
+      expect(nameLabel).toHaveTextContent('name');
+      expect(surnameLabel).toHaveTextContent('surname');
     });
   },
 };
 
 export const DefaultValues: Story = {
-  name: 'При передаче value, подставляются корректные данные',
+  name: 'По умолчанию отображаются значения из value',
   args: {
     onChange: fn(),
     schema: JSON.stringify(schema, null, 2),
@@ -60,19 +51,16 @@ export const DefaultValues: Story = {
       surname: 'Ivanych',
     },
   },
-  play: async ({ args, canvasElement, step }) => {
+  play: async ({ canvasElement, step }) => {
     const { getAllByTestId } = within(canvasElement);
     const items = getAllByTestId('props-editor__item');
 
-    const keys = getPropertiesNames(args.schema);
     await step('Ищем все поля и убеждаемся что значения соответствуют тем, что в пропсе', () => {
-      items.forEach((item, index) => {
-        const { getByTestId } = within(item);
-        const input: HTMLInputElement = getByTestId('props-editor__input');
-        const keyName = keys[index];
-        if (!keyName) throw Error('Не найден ключ с таким индексом');
-        expect(input.value).toBe(args.value[keyName]);
-      });
+      const nameInput: HTMLInputElement = within(items[0]!).getByTestId('props-editor__input');
+      const surnameInput: HTMLInputElement = within(items[1]!).getByTestId('props-editor__input');
+
+      expect(nameInput.value).toBe('Ivan');
+      expect(surnameInput.value).toBe('Ivanych');
     });
   },
 };
