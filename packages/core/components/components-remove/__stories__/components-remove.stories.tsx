6fc7d806d9ce0368fc428a632098219322cd36da
Ivan Podgornov <ivan.podgornov@live.com>
2024-04-26 19:54:25 +0300

    chore: добавлен декоратор с очисткой сообщений для тестов

--- packages/core/components/components-remove/__stories__/components-remove.stories.tsx
+++ packages/core/components/components-remove/__stories__/components-remove.stories.tsx
@@ -1,6 +1,8 @@
 import { exampleComponent } from '@colibrijs/mocks/components';
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
+import { message } from 'antd';
+import React from 'react';
 
 import type { WithMockedApi } from '../../../hooks/use-api/mocked';
 import { ComponentsRemove, type Props } from '../components-remove';
@@ -15,6 +17,14 @@ export default {
     component: exampleComponent,
     onRemove: fn(),
   },
+  decorators: [
+    (Story) => {
+      // Если убрать, сообщения будут сохраняться между сторисами. Не очень хорошо.
+      // Из-за этого одни тесты, могут повлиять на другие.
+      message.destroy();
+      return <Story />;
+    },
+  ],
 } satisfies ComponentsRemoveMeta;
 
 export const Default: Story = {};
