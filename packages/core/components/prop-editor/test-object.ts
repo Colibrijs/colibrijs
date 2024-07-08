d90e776de7d94f7218601647ecc6fac5fcc92643
okovtkov <okovtkov@gmail.com>
2024-07-08 21:07:25 +0200

    fix: test-object

--- packages/core/components/prop-editor/test-object.ts
+++ packages/core/components/prop-editor/test-object.ts
@@ -3,10 +3,12 @@ import { userEvent, within, type queries } from '@storybook/test';
 type ComponentElement = ReturnType<typeof within<typeof queries>>;
 
 export class PropEditorTO {
+  private editorElement: HTMLElement;
   private component: ComponentElement;
 
   constructor(canvasElement: HTMLElement, testId: string) {
-    this.component = within(within(canvasElement).getByTestId(testId));
+    this.editorElement = within(canvasElement).getByTestId(testId);
+    this.component = within(this.editorElement);
   }
 
   getPropertyName() {
@@ -17,7 +19,7 @@ export class PropEditorTO {
     return this.component.getByTestId('prop-editor__description');
   }
 
-  getInput() {
+  getInput(): HTMLInputElement {
     return this.component.getByTestId('prop-editor__input');
   }
 
@@ -30,8 +32,30 @@ export class PropEditorTO {
     await userEvent.click(switcher);
   }
 
-  async setValue(value: string) {
-    const input = this.getInput();
-    await userEvent.type(input, value);
+  getValue() {
+    const { type } = this.editorElement.dataset;
+
+    if (type === 'boolean') {
+      return this.getSwitcher().ariaChecked === 'true';
+    } else {
+      return this.getInput().value;
+    }
+  }
+
+  async setValue(value: string | boolean) {
+    const { type } = this.editorElement.dataset;
+
+    if (type === 'boolean') {
+      const switcher = this.getSwitcher();
+      if (value === true && switcher.ariaChecked === 'false') {
+        await userEvent.click(switcher);
+      }
+      if (value === false && switcher.ariaChecked === 'true') {
+        await userEvent.click(switcher);
+      }
+    } else {
+      const input = this.getInput();
+      await userEvent.type(input, value as string);
+    }
   }
 }
