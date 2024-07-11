47ca562813d5faa408f13ea3ade4afe9f72c44a5
okovtkov <okovtkov@gmail.com>
2024-07-11 21:18:18 +0200

    feat: создана getDefaultValues функция

--- packages/schema/index.ts
+++ packages/schema/index.ts
@@ -1,2 +1,3 @@
 export type { JsonSchema, SchemaValues, Property } from './types';
 export { getPropertiesNames } from './utils/get-properties-names';
+export { getDefaultValues } from './utils/get-default-values';
