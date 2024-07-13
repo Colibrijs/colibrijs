76282da954de292776ff28f69f8b5b7fdf1a2dbf
okovtkov <okovtkov@gmail.com>
2024-07-13 21:46:14 +0200

    fix: тесты

--- packages/schema/utils/get-default-values.spec.ts
+++ packages/schema/utils/get-default-values.spec.ts
@@ -8,27 +8,62 @@ const schema: JsonSchema<Record<string, SchemaValues>> = {
   $id: 'user',
   title: 'User',
   type: 'object',
-  properties: {
-    name: {
-      description: 'Your name',
-      type: 'string',
-    },
-    age: {
-      description: 'Your age',
-      type: 'number',
-    },
-    isBald: {
-      description: 'Your hair availability',
-      type: 'boolean',
-    },
-  },
+  properties: {},
 };
 
-describe('getDefaultValues', () => {
-  it('возвращает объект со значениями по умолчанию из свойства properties JSON-объекта переданного в параметр', () => {
+describe('stringValue', () => {
+  it('возвращает объект со значением у свойства в виде пустой строки, если type = "string"', () => {
+    expect.assertions(1);
+    const stringExample = { ...schema };
+    stringExample.properties = {
+      name: {
+        description: 'Your name',
+        type: 'string',
+      },
+    };
+    const defaultValues = getDefaultValues(stringExample);
+
+    expect(defaultValues).toStrictEqual({ name: '' });
+  });
+});
+
+describe('numberValue', () => {
+  it('возвращает объект со значением 0 у свойства, если type = "number"', () => {
+    expect.assertions(1);
+    const numberExample = { ...schema };
+    numberExample.properties = {
+      age: {
+        description: 'Your age',
+        type: 'number',
+      },
+    };
+    const defaultValues = getDefaultValues(numberExample);
+
+    expect(defaultValues).toStrictEqual({ age: 0 });
+  });
+});
+
+describe('booleanValue', () => {
+  it('возвращает объект со значением false у свойства, если type = "boolean"', () => {
+    expect.assertions(1);
+    const booleanExample = { ...schema };
+    booleanExample.properties = {
+      isBald: {
+        description: 'Your hair availability',
+        type: 'boolean',
+      },
+    };
+    const defaultValues = getDefaultValues(booleanExample);
+
+    expect(defaultValues).toStrictEqual({ isBald: false });
+  });
+});
+
+describe('emptyValue', () => {
+  it('возвращает пустой объект если properties значение у схемы - пустой объект', () => {
     expect.assertions(1);
     const defaultValues = getDefaultValues(schema);
 
-    expect(defaultValues).toStrictEqual({ name: '', age: 0, isBald: false });
+    expect(defaultValues).toStrictEqual({});
   });
 });
