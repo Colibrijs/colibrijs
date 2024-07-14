d5fe0d62186c3baa699b4c467770b1ff4cf85dc8
Ivan Podgornov <ext.ipodgornov@tinkoff.ru>
2024-07-14 16:35:24 +0300

    feat: реализация компонента element-add

---
+++ packages/core/components/element-add/__stories__/element-add.stories.tsx
@@ -0,0 +1,22 @@
+import { examplePage } from '@colibrijs/mocks/pages';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import type { WithMockedApi } from '../../../hooks/use-api/mocked';
+import { ElementAdd, type Props } from '../element-add';
+
+type ElementAddMeta = Meta<WithMockedApi<Props>>;
+type ElementAddStory = StoryObj<WithMockedApi<Props>>;
+
+export default {
+  component: ElementAdd,
+  title: 'ElementAdd',
+  args: {
+    open: true,
+    pageId: examplePage.id,
+    onClose: fn(),
+    onReady: fn(),
+  },
+} satisfies ElementAddMeta;
+
+export const Default: ElementAddStory = {};
