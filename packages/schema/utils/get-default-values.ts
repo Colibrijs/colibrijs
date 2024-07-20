868e6f1a4ead5377854406a190af02f6a49627bd
okovtkov <okovtkov@gmail.com>
2024-07-21 01:22:20 +0200

    feat: добавлена поддержка объектов для getDefaultValues

--- packages/schema/utils/get-default-values.ts
+++ packages/schema/utils/get-default-values.ts
@@ -1,12 +1,25 @@
-import type { JsonSchema, SchemaValues } from '../types';
+import type { JsonSchema, Property, SchemaValues } from '../types';
 
 export function getDefaultValues<T extends Record<string, SchemaValues>>(schema: JsonSchema<T>): T {
-  const keys = Object.keys(schema.properties);
-  const defaultValues = keys.reduce((obj: Record<string, SchemaValues>, key: string) => {
-    if (schema.properties[key]?.type === 'number') obj[key] = 0;
-    if (schema.properties[key]?.type === 'string') obj[key] = '';
-    if (schema.properties[key]?.type === 'boolean') obj[key] = false;
-    return obj;
-  }, {});
+  const defaultValues = getPropertiesValues(schema.properties);
   return defaultValues as T;
+}
+
+function getPropertiesValues<T extends Record<string, Property<SchemaValues>>>(
+  properties: T
+): Record<string, SchemaValues> {
+  const keys = Object.keys(properties);
+  return keys.reduce(
+    (obj, key) => {
+      if (properties[key]?.type === 'number') obj[key] = 0;
+      if (properties[key]?.type === 'string') obj[key] = '';
+      if (properties[key]?.type === 'boolean') obj[key] = false;
+      if (properties[key]?.type === 'object')
+        obj[key] = getPropertiesValues(
+          properties[key].properties as Record<string, Property<object>>
+        );
+      return obj;
+    },
+    {} as Record<string, SchemaValues>
+  );
 }
