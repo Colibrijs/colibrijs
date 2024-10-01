9a6dae64269813eff13da85e2707962969eb2b92
Oleg Kovtkov <66257346+okovtkov@users.noreply.github.com>
2024-10-01 23:32:00 +0200

    feat: добавлен зародыш будущего скриншот-теста (#126)
    * feat: добавлен зародыш будущего скриншот-теста
    * ci: example деплоится до запуска тестов
    ---------
    Co-authored-by: Ivan Podgornov

---
+++ packages/core/components/page-title/__stories__/screenshot/screenshot.stories.ts
@@ -0,0 +1,10 @@
+import PageTitleStoriesMeta, { type PageTitleMeta, type Story } from '../page-title.stories';
+
+export default {
+  ...PageTitleStoriesMeta,
+  title: 'PageTitle/tests/screenshot',
+} satisfies PageTitleMeta;
+
+export const Screenshot: Story = {
+  name: 'Заголовок с названием страницы и кнопка удаления страницы выровнены вертикально по центру',
+};
