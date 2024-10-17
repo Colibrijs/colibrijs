3dfe824d7de0e9a9124feab44707ae7f36c8d370
Ivan Podgornov <ext.ipodgornov@tinkoff.ru>
2024-10-18 01:36:54 +0300

    refactor(core): логика content-editor отделена от представления

--- packages/core/components/content-editor/__stories__/content-editor.stories.ts
+++ packages/core/components/content-editor/__stories__/content-editor.stories.ts
@@ -1,22 +1,35 @@
-import { exampleElements } from '@colibrijs/mocks/elements';
+import { exampleElement, exampleElements } from '@colibrijs/mocks/elements';
 import { examplePage } from '@colibrijs/mocks/pages';
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 
-import { SidebarDecorator } from '../../layout/sidebar-decorator';
+import { message } from 'antd';
+import React, { type ComponentProps } from 'react';
+
+import { withMockedApi, type WithMockedApi } from '../../../hooks/use-api/mocked';
 import { ContentEditor } from '../content-editor';
 
-export type StoryMeta = Meta<typeof ContentEditor>;
-export type Story = StoryObj<typeof ContentEditor>;
+export type StoryMeta = Meta<WithMockedApi<ComponentProps<typeof ContentEditor>>>;
+export type Story = StoryObj<WithMockedApi<ComponentProps<typeof ContentEditor>>>;
 
 export default {
   component: ContentEditor,
   title: 'ContentEditor',
-  decorators: [SidebarDecorator],
+  decorators: [
+    (StoryComponent) => {
+      message.destroy();
+      return React.createElement(StoryComponent);
+    },
+    withMockedApi((api) =>
+      api.override({
+        elements: {
+          get: () => Promise.resolve(exampleElements),
+          post: () => Promise.resolve(exampleElement),
+        },
+      })
+    ),
+  ],
   args: {
-    content: exampleElements,
-    pageId: examplePage.id,
-    onChange: fn(),
+    route: examplePage.id,
   },
 } satisfies StoryMeta;
 
