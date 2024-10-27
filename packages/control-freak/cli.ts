c53b695909db16ec8f200eaebf715ae6bf664d65
baronin <baronin.yaroslav@gmail.com>
2024-10-27 21:01:55 +0200

    refactor: баш скрипт переписан на ts

--- packages/control-freak/cli.ts
+++ packages/control-freak/cli.ts
@@ -1,8 +1,13 @@
 import { Command } from 'commander';
 
-interface Options {
+import { sendTelegramMessage } from './run';
+export interface Options {
   author: string;
   reviewer: string;
+  prTitle: string;
+  prUrl: string;
+  telegramBotToken: string;
+  telegramChatId: string;
 }
 
 const controlFreak = new Command();
@@ -11,9 +16,12 @@ controlFreak
   .command('request-review')
   .requiredOption('--author <string>', 'Имя автора пулл реквеста')
   .requiredOption('--reviewer <string>', 'Имя ревьюера пулл реквеста')
-  .action((options: Options) => {
-    // eslint-disable-next-line no-console -- для эксперимента надо
-    console.log(`${options.author} запрашивает ревью у ${options.reviewer}`);
+  .requiredOption('--pr-title <string>', 'Заголовок пулл реквеста')
+  .requiredOption('--pr-url <string>', 'Ссылка на пулл реквест')
+  .requiredOption('--telegram-bot-token <string>', 'Токен телеграм бота')
+  .requiredOption('--telegram-chat-id <string>', 'ID чата телеграм')
+  .action(async (options: Options) => {
+    await sendTelegramMessage(options);
   });
 
 controlFreak
